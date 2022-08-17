2fc683b148aad1b859c3deb582599a9f2051c7ab
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-08-16 23:27:47 -0400

    Add fastify websocket plugin and integrate route

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
-import fastify from 'fastify';
 
-const server = fastify({
+import fastify from 'fastify';
+import FastifyWebSocket from '@fastify/websocket';
+import socketRoutes from './connection';
+
+const app = fastify({
   logger: true,
 });
 
-server.get('/', async () => 'Hello');
+app.register(FastifyWebSocket);
+app.register(socketRoutes);
 
-server.listen({ port: 8080 }, (error, address) => {
+app.listen({ port: 8080 }, (error, address) => {
   if (error) {
     console.error(error);
     process.exit(1);
