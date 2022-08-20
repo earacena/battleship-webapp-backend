5d06c265819489daefc2c6ef0c34f1b481ac350c
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-08-19 21:54:50 -0400

    Implement keeping track of clients

--- src/connection.ts
+++ src/connection.ts
@@ -1,12 +1,30 @@
 import type { SocketStream } from '@fastify/websocket';
 import type { FastifyInstance } from 'fastify';
 
+const Clients: Map<number, SocketStream> = new Map<number, SocketStream>();
+let clientCount: number = 0;
+
 async function socketRoutes(fastify: FastifyInstance) {
   fastify.get('/', { websocket: true }, (connection: SocketStream) => {
+    // Connection established, send user client an Id
+    const messageJSON: string = JSON.stringify({ type: 'id', message: `${clientCount}` });
+    Clients.set(clientCount, connection);
+    clientCount += 1;
+    console.log(`clients: ${Clients}, # of clients: ${clientCount}`);
+    Clients.forEach((_socket) => {
+      _socket.socket.send(JSON.stringify({ type: 'message', message: `${clientCount-1} has joined` }));
+    });
+
+    connection.socket.send(messageJSON);
+
     connection.socket.on('message', (message: string) => {
       if (message.toString() === 'hi from client') {
-        console.log('hi from server');
-        connection.socket.send('hi from server');
+        console.log(message.toString());
+        const response: string = JSON.stringify({
+          type: 'message',
+          message: 'hi from server',
+        });
+        connection.socket.send(response);
       }
     });
   });
