1e9bed1875b06bf5148a647a7c3b6847ce3530e9
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-08-22 01:32:52 -0400

    Implement message parsing and handling

--- src/Connection.ts
+++ src/Connection.ts
@@ -1,8 +1,11 @@
 import { v4 as uuidv4 } from 'uuid';
-
 import type { SocketStream } from '@fastify/websocket';
+import type { MessageType } from './connection.types';
+import { zString, Message } from './connection.types';
+import Queue from './Queue';
 
 const Clients: Map<string, SocketStream> = new Map<string, SocketStream>();
+const queue: Queue = new Queue();
 
 class Connection {
   id: string;
@@ -17,14 +20,25 @@ class Connection {
 
     this.connection.socket.send(messageJSON);
 
-    this.connection.socket.on('message', (message: string) => {
-      if (message.toString() === 'hi from client') {
-        console.log(message.toString());
-        const response: string = JSON.stringify({
-          type: 'message',
-          message: 'hi from server',
-        });
-        connection.socket.send(response);
+    this.connection.socket.on('message', (data: Buffer) => {
+      const message: any = JSON.parse(zString.parse(data.toString()));
+    
+      if (message.hasOwnProperty('type')) {
+        switch (message.type) {
+          case 'message':
+            const parsedMessage: MessageType = Message.parse(message);
+            console.log(parsedMessage);
+            break;
+          case 'enqueue user':
+            queue.enqueue(this.id);
+            console.log(`current person next in line at queue: ${queue.front()}`);
+
+            // Notify user that they were queued
+            const messageJSON: string = JSON.stringify({ type: 'queued user' });
+            this.connection.socket.send(messageJSON);
+        };
+      } else {
+        console.error(`malformatted websocket message received: ${message}`)
       }
     });
 
