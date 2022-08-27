65cae92f7f47f51c9f933b8a66f083401c1c92ee
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-08-27 13:19:06 -0400

    Add type for messages containing shot position

--- src/connection.types.ts
+++ src/connection.types.ts
@@ -7,4 +7,13 @@ export const Message = z.object({
   message: z.string(),
 });
 
+export const PlayerFiredMessage = z.object({
+  type: z.string(),
+  position: z.object({
+    y: z.number(),
+    x: z.number(),
+  }),
+});
+
 export type MessageType = z.infer<typeof Message>;
+export type PlayerFiredMessageType = z.infer<typeof PlayerFiredMessage>;
