fc577e538f3881f2b9f39690cc8d28da3d245c4a
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-09-03 17:50:37 -0400

    Combine regular and websocket enabled routes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
 
-import fastify from 'fastify';
-import FastifyWebSocket from '@fastify/websocket';
-import socketRoutes from './socketRoutes';
-
+import fastify, { FastifyRequest, FastifyReply } from 'fastify';
+import FastifyWebSocket, { SocketStream } from '@fastify/websocket';
+import FasififyStatic from '@fastify/static';
+//import socketRoutes from './socketRoutes';
+import path from 'path';
+import Connection from './Connection';
 const app = fastify({
   logger: false,
 });
-
 app.register(FastifyWebSocket);
-app.register(socketRoutes);
+app.register(FasififyStatic, {
+  root: path.join(__dirname, 'build_frontend'),
+});
+// app.register(socketRoutes);
+
+app.route({
+  method: 'GET',
+  url: '/hello',
+  handler: (_request: FastifyRequest, reply: FastifyReply) => {
+    reply.sendFile('index.html');
+  },
+  wsHandler: (connection: SocketStream, _request: FastifyRequest) => {
+    // Connection established, send user client an Id
+    const conn = new Connection(connection);
+    console.log(`user connected: ${conn.id}`);
+  }
+})
 
 app.listen({ port: 8080 }, (error, address) => {
   if (error) {
